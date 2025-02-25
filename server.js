32031ac20c286ce2c9d58ca1a0e36ab0e1c4192c
JHOER_S_F <jhoerforero102003@gmail.com>
2025-02-25 09:59:01 -0500

    commit problema en server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
-const port = process.env.PORT || 3000 ;
+const port = 3000 ;
 
 const app = express();
 app.use(express.json());
@@ -16,8 +16,8 @@ const pacienteRoutes = require('./routes/pasienteRoutes');
 const medicoRoutes = require('./routes/medicoRoutes');
 const citaRoutes = require('./routes/citaRoutes');
 
-app.get("/", (req,res)=>{
-    res.json("pagina principal")
+app.(req,res)=>{
+    ("pagina principal")
 })
 app.use('/pacientes', pacienteRoutes);
 app.use('/medicos', medicoRoutes);
